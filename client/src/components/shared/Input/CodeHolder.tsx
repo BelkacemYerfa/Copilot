886ef93c19ac25f15121eefe33b6a8289dd0706d
belkacem <belkacem.yerfa@gmail.com>
2023-04-15 01:16:56 +0100

    fixing the triger functionality for the input fields to get all their values

--- client/src/components/shared/Input/CodeHolder.tsx
+++ client/src/components/shared/Input/CodeHolder.tsx
@@ -1,33 +1,48 @@
-import { ChangeEvent, KeyboardEvent } from "react";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
 import debounce from "lodash.debounce";
 
 type CodeHolder = {
   code: string[];
 };
 
+type UserCode = string[];
+
+interface NewCode {
+  newCode: string[];
+  codeChange: (newCode: string[]) => void;
+}
+
 const CodeHolder = ({ code }: CodeHolder) => {
+  const [newCode, setNewCode] = useState<UserCode>([]);
+  console.log(newCode);
   return (
     <div className="flex items-center gap-x-2">
       {code.map((item, index) => (
-        <Input key={index} />
+        <Input key={index} newCode={newCode} codeChange={setNewCode} />
       ))}
     </div>
   );
 };
 
-const Input = () => {
+const Input = ({ newCode, codeChange }: NewCode) => {
   const Pass_To_Next_Element = debounce((e: ChangeEvent) => {
     const target = e.target as HTMLInputElement;
     const next = target.nextElementSibling as HTMLInputElement;
+
     if (target.value.length > 1) {
       target.value = target.value[0];
+      codeChange([...newCode, target.value]);
       next?.focus();
     }
   }, 200);
   const Return_To_Previous_Element = (e: KeyboardEvent<HTMLInputElement>) => {
     const target = e.target as HTMLInputElement;
     const previous = target.previousElementSibling as HTMLInputElement;
-    if (target.value.length === 0) previous?.focus();
+    if (target.value.length === 0) {
+      newCode.slice(0, newCode.length);
+      codeChange(newCode);
+      previous?.focus();
+    }
     const next = target.nextElementSibling as HTMLInputElement;
     if (e.key === "ArrowLeft") previous?.focus();
     if (e.key === "ArrowRight") next?.focus();
